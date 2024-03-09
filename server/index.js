cd02eaf21e33f5d7a9c4a5a56a098e0ce4f368de
Aiman Fadillah <mlakece914@gmail.com>
2024-03-09 09:09:34 +0700

    finish setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
-app.listen(5000,() => console.log("Server on"))
+app.get("*", (req, res) => res.sendFile(__dirname + "/index.html"))
+
+app.listen(5000, () => console.log("Server on"))
