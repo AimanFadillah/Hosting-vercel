eb0df088cb293932e783194200e20b0fb688c8d9
Aiman Fadillah <mlakece914@gmail.com>
2024-03-15 21:40:16 +0700

    test upload image

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
+app.use(fileUpload());
+app.use(cors());
 app.use(express.static(__dirname + "/public"));
+
+app.post("/upload", (req, res) => {
+    const file = req.files.file;
+    file.mv(`./public/img/${file.name}`);
+    return res.json(file.name);
+})
 
 app.get("*", (req, res) => res.sendFile(__dirname + "/index.html"))
 
